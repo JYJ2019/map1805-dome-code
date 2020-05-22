bfd005a0bda8d983ed2eca6080f693d51e6e442e
JYJ <1036470143@qq.com>
2020-05-22 10:28:35 +0800

    修改

--- myyk-code/src/com/it/code/dome01/Test04.java
+++ myyk-code/src/com/it/code/dome01/Test04.java
@@ -15,7 +15,7 @@ public class Test04 {
         }
 
         for (int i = 0; i < array.length - 1; i++) {
-            for (int j = 0; j < array.length - 1 -i; j++) {
+            for (int j = 0; j < array.length - 1 - i; j++) {
                 if (array[j] < array[j + 1]) {
                     int temp = array[j];
                     array[j] = array[j + 1];
